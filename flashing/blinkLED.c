86f669d7a74271e121660c446c3ed60337cd35e6
akhilsankar1993 <akhil.sankar@wink.com>
2018-10-20 17:24:55 -0400

    some small updates

--- flashing/blinkLED.c
+++ flashing/blinkLED.c
@@ -7,20 +7,21 @@
 
 int main(void) {
 
-// -------- Inits --------- //
-DDRB |= 0b00000001;            /* Data Direction Register B:
-writing a one to the bit
-enables output. */
+    // -------- Inits --------- //
+    DDRB |= 0b00000001;            /* Data Direction Register B:
+    writing a one to the bit
+    enables output. */
 
-// ------ Event loop ------ //
-while (1) {
+    // ------ Event loop ------ //
+    while (1) {
 
-PORTB = 0b00000001;          /* Turn on first LED bit/pin in PORTB */
-_delay_ms(2000);                                           /* wait */
+        PORTB = 0b00000001;          /* Turn on first LED bit/pin in PORTB */
+        _delay_ms(1000);                                           /* wait */
 
-PORTB = 0b00000000;          /* Turn off all B pins, including LED */
-_delay_ms(1000);                                           /* wait */
+        PORTB = 0b00000000;          /* Turn off all B pins, including LED */
+        _delay_ms(3000);                                           /* wait */
 
-}                                                  /* End event loop */
-return 0;                            /* This line is never reached */
+    } 
+                                                     /* End event loop */
+    return 0;                            /* This line is never reached */
 }
